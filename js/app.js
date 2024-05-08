996e2cceaaa47b4d533305916ca09fe8677df3cb
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-05-08 12:06:42 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.querySelector('.top>h1').textContent = 'Join our community';
+document.querySelector('.top>h3').textContent = `30-day, hassle-free money back guarantee`;
+document.querySelector('.top>p').textContent =`Gain access to our full library of tutorials along with expert code reviews.
+                                                Perfect for any developers who are serious about honing their skills.`;
+document.querySelector('.left>p').textContent = `Monthly Subscription`;
+document.querySelector('.middle>span:first-child').textContent = `$29`;
+document.querySelector('.middle>span:last-child').textContent = `per month`;
+document.querySelector('.last-p').textContent = `Full access for less than $1 a day`;
+document.querySelector('button>a').textContent = `Sign Up`;
+document.querySelector('.last-head').textContent = `Why Us`;
+document.querySelector('.last-para').innerText = `Tutorials by industry experts
+          Peer & expert code review
+          Coding exercises
+          Access to our GitHub repos
+          Community forum 
+          Flashcard decks
+          New videos every week
+        `;
+document.querySelector('p>a:first-child').innerText = `CodeTribe`;
+document.querySelector('p>a:last-child').innerText = `Moropane Kutullo Innocent and https://ikcracker.github.io/Intro-to-HTML/`;
