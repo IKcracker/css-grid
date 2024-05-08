fc3e57dd4cdef3a70abdd3754e32cefbe96b073b
Kutullo innocent Moropane <49004939+IKcracker@users.noreply.github.com>
2024-05-08 12:00:30 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.querySelector('.top>h1').textContent = 'Join our community';
-document.querySelector('.top>h3').textContent = `30-day, hassle-free money back guarantee`
+document.querySelector('.top>h3').textContent = `30-day, hassle-free money back guarantee`;
 document.querySelector('.top>p').textContent =`Gain access to our full library of tutorials along with expert code reviews.
                                                 Perfect for any developers who are serious about honing their skills.`;
 document.querySelector('.left>p').textContent = `Monthly Subscription`;
@@ -15,7 +15,6 @@ document.querySelector('.last-para').innerText = `Tutorials by industry experts
           Community forum 
           Flashcard decks
           New videos every week
-
-document.querySelector('footer>p>a:first-child').textContent = `CodeTribe`;
-document.querySelector('footer>p>a:last-child').textContent = `Moropane Kutullo Innocent and https://ikcracker.github.io/Intro-to-HTML/`;
-
+        `;
+document.querySelector('p>a:first-child').innerText = `CodeTribe`;
+document.querySelector('p>a:last-child').innerText = `Moropane Kutullo Innocent and https://ikcracker.github.io/Intro-to-HTML/`;
